c336ed11f57d04b09b87235e1073f4606042aa88
unknown <zhutiantian@ciicsh.local>
2019-04-25 14:42:25 +0800

    jj

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,44 @@
 module.exports = {
   themeConfig: {
+    sidebar: {
+      '/foo/': [
+        '',     /* /foo/ */
+        'one',  /* /foo/one.html */
+        'two'   /* /foo/two.html */
+      ],
+
+      '/bar/': [
+        '',      /* /bar/ */
+        'three', /* /bar/three.html */
+        'four'   /* /bar/four.html */
+      ],
+
+      // fallback
+      '/': [
+        '',        /* / */
+        'contact', /* /contact.html */
+        'about'    /* /about.html */
+      ]
+    },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
+      { text: 'java', link: '/foo/' },
+      { text: 'springboot', link: '/springboot/' },
+      { text: 'javascript', link: 'javascript' },
+      { text: 'css', link: 'css' },
+      { text: 'html', link: 'html' },
+      { text: 'GitHub', link: 'GitHub' },
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Chinese', link: '/language/chinese' },
+          { text: 'English', link: '/language/english' }
+        ]
+      }
     ]
   },
+  title: '小猪学习',
+  description : '每天一点',
+  lastUpdated: 'Last Updated',
   base: '/bar/',
   configureWebpack: {
     resolve: {
